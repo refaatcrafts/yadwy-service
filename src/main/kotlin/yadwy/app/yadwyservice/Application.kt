edea8b61d7b061aada1a04a8f4642cd10fba0fa2
Refaat <hassaan.refaat@taager.com>
2025-12-07 09:01:51 +0200

    refactor: rename YadwayApplication to Application and update all references accordingly

--- src/main/kotlin/yadwy/app/yadwyservice/Application.kt
+++ src/main/kotlin/yadwy/app/yadwyservice/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class YadwyServiceApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<YadwyServiceApplication>(*args)
+    runApplication<Application>(*args)
 }
