b6213b9700a8c1a2da73e64d722aa35a5a84704f
Refaat <hassaan.refaat@taager.com>
2026-01-11 19:58:25 +0200

    chore(security/observability): update configs and clean up Grafana LGTM stack
    - Allow unauthenticated access to `/actuator/health` endpoint in `SecurityConfig`.
    - Remove Grafana LGTM stack setup and related environment variables from Docker Compose.
    - Update OTLP endpoints and add authorization headers for production environment in `application.yml`.
    - Add `dev` environment configuration to `http-client.env.json`.

--- src/main/kotlin/yadwy/app/yadwyservice/identity/infrastructure/security/SecurityConfig.kt
+++ src/main/kotlin/yadwy/app/yadwyservice/identity/infrastructure/security/SecurityConfig.kt
@@ -31,6 +31,7 @@ class SecurityConfig(
                     .requestMatchers("/swagger-ui.html/**").permitAll()
                     .requestMatchers("/swagger-ui/**").permitAll()
                     .requestMatchers("/v3/api-docs/**").permitAll()
+                    .requestMatchers("/actuator/health").permitAll()
                     .anyRequest().authenticated()
             }
             .oauth2ResourceServer { oauth2 -> oauth2.jwt {} }
